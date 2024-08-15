55d6993c57f867daaf495956834d796cb92dd60b
Jose Tomas Hernandez Lopez <josetomas.hlopez@gmail.com>
2024-08-15 13:21:30 +0200

    fixed config & linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: "@/", replacement: "/src/" }],
+  },
+  envPrefix: "VITE_",
+  build: {
+    outDir: "dist",
+    assetsDir: "",
+    sourcemap: false,
+    minify: true,
+    manifest: true,
   },
 });
