81e5ccb560590c45f35be2b1f421fc58e3e3f6a5
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2025-07-25 14:42:21 +0200

    remove unnecesary code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
-import CustomContextMenu from './context/rightClickMenuContext';
 import './main.scss';
 import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <CustomContextMenu />
   </StrictMode>
 );
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,7 @@
 import Title from '@/components/Title';
-import { useTitle } from '@/hooks/useTitle';
 import styles from './styles.module.scss';
 
 export default function AboutPage() {
-  useTitle('About Me');
-
   return (
     <div>
       <div className={styles.container}>
