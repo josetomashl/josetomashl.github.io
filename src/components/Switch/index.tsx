82878bff1c9cdba17f70ae370c99a250d277a006
José Tomás Fiscoo <josetomas.hl@fiscoo.es>
2025-07-29 11:31:15 +0200

    apply changes

---
+++ src/components/Switch/index.tsx
@@ -0,0 +1,34 @@
+import { useId } from 'react';
+import styles from './styles.module.scss';
+
+type ToggleProps = {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+  disabled?: boolean;
+  label?: string;
+};
+
+export default function Switch({ checked, onChange, disabled = false, label = '' }: ToggleProps) {
+  const id = useId();
+
+  const handleToggle = () => {
+    if (!disabled) {
+      onChange(!checked);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <input type='checkbox' id={id} checked={checked} onChange={handleToggle} disabled={disabled} />
+      <span
+        className={`${styles.track} ${checked ? styles.checked : ''} ${disabled ? styles.disabled : ''}`}
+        style={{ cursor: disabled ? 'not-allowed' : 'pointer' }}
+        onClick={handleToggle}>
+        <span className={styles.thumb} />
+      </span>
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+    </div>
+  );
+}
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,9 +1,13 @@
 import Dropdown, { type DropdownOption } from '@/components/Dropdown';
+import Icon from '@/components/Icon';
+import Switch from '@/components/Switch';
 import Title from '@/components/Title';
 import type { LocaleType } from '@/context/languageContext';
+import useTheme from '@/hooks/useTheme';
 import { useTitle } from '@/hooks/useTitle';
 import useTranslations from '@/hooks/useTranslations';
 import { useState } from 'react';
+import styles from './styles.module.scss';
 
 const availableLanguages = [
   { label: 'language_en', value: 'en', icon: '/icons/united_kingdom.svg' },
@@ -12,30 +16,46 @@ const availableLanguages = [
 
 export default function SettingsPage() {
   useTitle('Preferences');
-  const { setLanguage, language } = useTranslations();
 
-  const [item, setItem] = useState<DropdownOption>(
+  const { setLanguage, language } = useTranslations();
+  const [langOption, setLangOption] = useState<DropdownOption>(
     () => availableLanguages.find((lang) => lang.value === language) || availableLanguages[0]
   );
-
   const handleLanguageChange = (value: DropdownOption) => {
-    setItem(value);
+    setLangOption(value);
     setLanguage(value.value as LocaleType);
+  };
+
+  const { theme, setTheme } = useTheme();
+  // const [darkTheme, setDarkTheme] = useLocalStorage('THEME', 'dark');
+  const handleThemeChange = (value: boolean) => {
+    setTheme(value ? 'dark' : 'light');
+    // setDarkTheme(value ? 'dark' : 'light');
   };
 
   return (
     <div>
       <Title content='Preferences' />
 
-      <h3>General Settings</h3>
-      <div>
-        <h4>Language</h4>
+      <div className={styles.container}>
+        <h4>
+          <Icon name='globe' size={20} />
+          Language
+        </h4>
         <Dropdown
           options={availableLanguages}
-          value={item}
+          value={langOption}
           onChange={handleLanguageChange}
           label='Choose your preferred language'
         />
+      </div>
+
+      <div className={styles.container}>
+        <h4>
+          <Icon name='brush' size={20} />
+          Theme
+        </h4>
+        <Switch checked={theme === 'dark'} onChange={handleThemeChange} label='Dark mode' />
       </div>
     </div>
   );
