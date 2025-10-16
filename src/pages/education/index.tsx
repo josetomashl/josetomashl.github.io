ea5766bf914f6b320c18b6e91fd295733a60af05
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2025-10-16 14:59:41 +0200

    add sorter

--- src/pages/education/index.tsx
+++ src/pages/education/index.tsx
@@ -1,4 +1,4 @@
-import { Activity } from 'react';
+import { Activity, useState } from 'react';
 
 import Subtitle from '@/components/common/Subtitle';
 import Title from '@/components/common/Title';
@@ -13,6 +13,8 @@ import styles from './styles.module.scss';
 export default function EducationPage() {
   useTitle('Education');
 
+  const [sort, setSort] = useState('date');
+
   return (
     <Activity mode='visible'>
       <Title content='Training & Courses' />
@@ -23,11 +25,31 @@ export default function EducationPage() {
         .map((lang) => (
           <LanguageCard key={lang.name + lang.qualification} data={lang} />
         ))}
-      <Subtitle content='Certifications' />
+      <div className={styles.certificationsHeader}>
+        <Subtitle content='Certifications' />
+        <div>
+          <label htmlFor='sorting_selector' style={{ marginRight: '0.5rem' }}>
+            Sort by:
+          </label>
+          <select id='sorting_selector' value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value='date'>Year</option>
+            <option value='relevance'>Relevance</option>
+          </select>
+        </div>
+      </div>
       <div className={styles.certificationsContainer}>
-        {CERTIFICATIONS.map((cert) => (
-          <CertificationCard key={cert.link + '_' + cert.name.replaceAll(' ', '-')} data={cert} />
-        ))}
+        {CERTIFICATIONS.slice()
+          .sort((a, b) => {
+            if (sort === 'date') {
+              return b.year - a.year;
+            } else {
+              const relevanceOrder = { high: 3, medium: 2, low: 1 };
+              return relevanceOrder[b.relevance] - relevanceOrder[a.relevance];
+            }
+          })
+          .map((cert) => (
+            <CertificationCard key={cert.link + '_' + cert.name.replaceAll(' ', '-')} data={cert} />
+          ))}
       </div>
     </Activity>
   );
